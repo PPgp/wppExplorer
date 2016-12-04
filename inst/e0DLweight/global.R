6af9c592c83c16d9d46df19465900a1abcad69ca
Hana Sevcikova <hanas@uw.edu>
2016-12-03 19:10:28 -0800

    dir edited

--- inst/e0DLweight/global.R
+++ inst/e0DLweight/global.R
@@ -1,6 +1,6 @@
 library(bayesLife)
-sim.dir <- "~/talks/UN16/e0/converged/e0/sim03092016/"
-#sim.dir <- '/mnt/shinydata/apps/wppExplorer/e0simulation'
+#sim.dir <- "~/talks/UN16/e0/converged/e0/sim03092016/"
+sim.dir <- '/mnt/shinydata/apps/wppExplorer/e0simulation'
 e0.pred <<- get.e0.prediction(sim.dir)
 DLx <<- seq(20, 90, length=100) 
 dl.world <- e0.world.dlcurves(DLx, e0.pred)
